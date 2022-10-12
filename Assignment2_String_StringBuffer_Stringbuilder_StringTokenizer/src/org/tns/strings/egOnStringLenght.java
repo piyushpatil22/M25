967e94737074b6b6f1d25792fd17427573f0b433
Piyush Patil <piyushpatilssp@gmail.com>
2022-10-12 15:22:54 +0530

    Assignment 2 update

---
+++ Assignment2_String_StringBuffer_Stringbuilder_StringTokenizer/src/org/tns/strings/egOnStringLenght.java
@@ -0,0 +1,10 @@
+package org.tns.strings;
+
+public class egOnStringLenght {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Assignment2_String_StringBuffer_Stringbuilder_StringTokenizer/src/org/tns/stringtokenizer/egOnStringTokenizer.java
@@ -0,0 +1,10 @@
+package org.tns.stringtokenizer;
+
+public class egOnStringTokenizer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
